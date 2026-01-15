b3d168663640c26c200be2fb6d7e9e2c733cb1f6
RafyHossain <rafyhossain56@gmail.com>
2026-01-16 02:43:02 +0600

    enrolment api

--- index.js
+++ index.js
@@ -79,6 +79,70 @@ async function run() {
       }
     });
 
+    app.get("/my-courses/:email", async (req, res) => {
+      const result = await courseCollection
+        .find({ "instructor.email": req.params.email })
+        .toArray();
+      res.send(result);
+    });
+
+    app.put("/courses/:id", async (req, res) => {
+      const result = await courseCollection.updateOne(
+        { _id: new ObjectId(req.params.id) },
+        { $set: req.body }
+      );
+      res.send(result);
+    });
+
+    app.delete("/courses/:id", async (req, res) => {
+      const result = await courseCollection.deleteOne({
+        _id: new ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
+
+    /* ENROLL  */
+
+    
+    app.post("/enroll", async (req, res) => {
+      const { courseId, studentEmail } = req.body;
+
+      const exists = await enrollCollection.findOne({
+        courseId,
+        studentEmail,
+      });
+
+      if (exists) {
+        return res.send({ enrolled: true });
+      }
+
+      const result = await enrollCollection.insertOne({
+        courseId,
+        studentEmail,
+        enrolledAt: new Date(),
+      });
+
+       // get enroll ment
+    app.get("/enroll/:email", async (req, res) => {
+      const result = await enrollCollection
+        .find({ studentEmail: req.params.email })
+        .toArray();
+      res.send(result);
+    });
+
+
+      //  increment enrolledCount
+      await courseCollection.updateOne(
+        { _id: new ObjectId(courseId) },
+        { $inc: { enrolledCount: 1 } }
+      );
+
+      res.send(result);
+    });
+
+   
+    // unenrolment
+   
   } finally {
     
   }
